d23b26309f3c751292c110221055965203b0f669
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-03-04 19:16:28 -0600

    main menu button working

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from 'react-router-dom';
 
 function Details() {
     const dispatch = useDispatch();
     const detailsArr = useSelector(store => store.details.data);
+    const history = useHistory();
 
     console.log('MOVIE DETAILS', detailsArr);
 
@@ -21,7 +22,7 @@ function Details() {
                             <p>{detail.description}</p>
                             <h3>Genres</h3>
                             <p>{detail.genres}</p>
-                            <button onClick={() => mainMenu()}>Main Menu</button>
+                            <button onClick={() => history.push('/')}>Main Menu</button>
                         </div>
                     );
                 })}
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import './MovieList.css';
 
